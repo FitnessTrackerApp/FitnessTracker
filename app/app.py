6190b164884a427093ff31247a61dd27a2229028
Hikmet Kaan Oktay <kaan.oktay@ug.bilkent.edu.tr>
2024-05-01 18:29:05 +0300

    .

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+#from flask_mysqldb import MySQL
+#import MySQLdb.cursors
 
 app = Flask(__name__)
 
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 @app.route('/')
 def index():
