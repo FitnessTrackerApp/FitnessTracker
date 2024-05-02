738b37a9d860cb52fe9dc3bbd772bd7173884a40
Serhat Yılmaz <91696123+SSerhatS@users.noreply.github.com>
2024-05-02 03:51:15 +0300

    docker works
    fix: now docker works correctly.

--- app/app.py
+++ app/app.py
@@ -5,6 +5,13 @@ import MySQLdb.cursors
 
 app = Flask(__name__)
 
+app.secret_key = 'abcdefgh'
+
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'cs353hw4db'
+
 mysql = MySQL(app)
 
 @app.route('/')
@@ -124,4 +131,5 @@ def req_prog():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
